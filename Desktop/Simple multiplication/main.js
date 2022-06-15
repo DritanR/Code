6748d9812e76095e04da37355d921193db0cfa83
DritanR <dritanrexhepi04@gmail.com>
2022-06-15 06:55:16 -0700

    Simple math

---
+++ Desktop/Simple multiplication/main.js
@@ -0,0 +1,8 @@
+//If number is even multiply it by 9. If not multiply it by 8.
+function multiply (number) {
+    if (number%2===0) {
+       return number*9
+    }else {
+       return number*8
+    }
+}
